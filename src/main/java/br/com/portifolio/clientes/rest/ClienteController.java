9205951863fcadb0c01afa1e7bd0229cc979e024
monterohv <lucmontero@gmail.com>
2021-06-07 16:57:25 -0300

    Update ClienteController.java

--- src/main/java/br/com/portifolio/clientes/rest/ClienteController.java
+++ src/main/java/br/com/portifolio/clientes/rest/ClienteController.java
@@ -24,7 +24,7 @@ import br.com.portifolio.clientes.model.repository.ClienteRepository;
 @RestController
 @RequestMapping("/api/clientes")
 //@CrossOrigin("http://localhost:4200")
-@CrossOrigin("https://springboot.sp.skdrive.net")
+@CrossOrigin("http://springboot.sp.skdrive.net")
 public class ClienteController {
 	
 	@Autowired
