0d57e1ac8d510efaa5da3a1654b74ed22a198a50
monterohv <lucmontero@gmail.com>
2021-06-08 01:13:48 -0300

    Build

--- src/main/java/br/com/portifolio/clientes/rest/ClienteController.java
+++ src/main/java/br/com/portifolio/clientes/rest/ClienteController.java
@@ -6,7 +6,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,8 +22,6 @@ import br.com.portifolio.clientes.model.repository.ClienteRepository;
 
 @RestController
 @RequestMapping("/api/clientes")
-//@CrossOrigin("http://localhost:4200")
-@CrossOrigin("http://springboot.sp.skdrive.net")
 public class ClienteController {
 	
 	@Autowired
